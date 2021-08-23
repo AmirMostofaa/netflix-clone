78633bd8e48cf4972e539cfb26db34c2f6a27ffa
amir <iamamirmostafa@gmail.com>
2021-08-23 08:24:00 +0600

    update 2.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![netflix_img](https://i.ibb.co/wsgvMHk/netflix-min.jpg).
 
-# [LIVE](https://netflix-clone-by-amir.web.app/).
+# [LIVE](https://netflix-clone-by-amir.web.app/)
 
 See the Live site.
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className="footer">
-            Netflix Cloned with ❤️ by <a href="https://amirmostofaa.netlify.app" target="_blank" rel="noreferrer">Amir</a>
+            Netflix Cloned with ❤️ by <a href="https://mostafamahmud.netlify.app" target="_blank" rel="noreferrer">Mostafa</a>
         </div>
     );
 };
